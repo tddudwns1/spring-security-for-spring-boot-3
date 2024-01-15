e6c80f894a518a6e7ff74b3510e574ab693cc7ab
tddudwns1 <tjddudwns1@naver.com>
2024-01-15 23:58:45 +0900

    :sparkles: url path 를 통한 접근 제어
    SpringSecurityConfig에 requestMathcers로 path를 지정하고, hasRole을 통해 해당 권한을 가져야 접근 가능함을 설정

--- src/main/java/com/nahwasa/springsecuritybasicsettingforspringboot3/service/RegisterMemberService.java
+++ src/main/java/com/nahwasa/springsecuritybasicsettingforspringboot3/service/RegisterMemberService.java
@@ -13,12 +13,6 @@ public class RegisterMemberService {
     private final PasswordEncoder passwordEncoder;
     private final MemberRepository repository;
 
-    @Autowired
-    public RegisterMemberService(PasswordEncoder passwordEncoder, MemberRepository repository) {
-        this.passwordEncoder = passwordEncoder;
-        this.repository = repository;
-    }
-
     public Long join(String userid, String pw) {
         Member member = Member.createUser(userid, pw, passwordEncoder);
         validateDuplicateMember(member);
